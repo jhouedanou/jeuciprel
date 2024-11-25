9b1e6ee64a67599ce24a920f6d5028edf27f9155
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-11-25 11:41:59 +0000

    ach

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ css: [
     },
   },
   head: {
-    title:"DINOR APP - Calculateur de calories",  meta: [
+    title:"CIPREL Puzzle",  meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
